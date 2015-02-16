4b379824bd35990f64b03c6c43cf150e09f64430
Michael Schmidt <mschmidt74@hotmail.com>
2015-02-16 07:44:40 -0500

    Update CreateMatch.blade.php
    Clean up white space

--- app/views/CreateMatch.blade.php
+++ app/views/CreateMatch.blade.php
@@ -32,8 +32,9 @@
         <script src="<?php echo asset('jquerymultiselect/jquery.multiselect.min.js')?>" type="text/javascript"></script>
         <script>
             $("#date").datepicker();
-			
-			$(document).ready(function() {                
+		var players = [];
+		var levels =[];
+		$(document).ready(function() {
                 $.getJSON("{{URL::to('/')}}/courses", function(data){
                     $.each(data, function(index, text) {
                     $("#course").append(
@@ -41,40 +42,39 @@
                         );
                     });
                 });
-				$.getJSON("{{URL::to('/')}}/players", function(data){
-					players = data;                    
+		$.getJSON("{{URL::to('/')}}/players", function(data){
+			players = data;
                 });
-				$.getJSON("{{URL::to('/')}}/levels", function(data){
+		$.getJSON("{{URL::to('/')}}/levels", function(data){
                     levels = data;
                 });
-				for (i = 12; i > 0; i--) {
-					$("#players").append($("<option></option>").val(i).html(i));
-				}
-				var groups = [1,2,3];
-				
-				$("#players")
-				  .change(function () {
-						$('#playersList').empty();
-						for (i = $(this).val(); i > 0; i--) {
-							$('#playersList').append('<div name="player"><label for="player">Player</label><select name="player_id[]" class="player ui-corner-all" id="player"></select><label for="level">Level</label><select name="level_id[]" class="level ui-corner-all" id="level" ></select><label for="group">Group</label><select name="group[]" class="group ui-corner-all" id="group" ></select></div><br />'); //add player div
-						}
-						$.each(players, function(index, text) {
-							$(".player").append(
-								$("<option></option>").val(text.id).html(text.name)
-							);
-						});
-						$.each(levels, function(index, text) {
-							$(".level").append(
-								$("<option></option>").val(text.id).html(text.name)
-							);
-						});
-						for (i = 0; i < groups.length; i++) {
-							$(".group").append(
-								$("<option></option>").val(groups[i]).html(groups[i])
-							);
-						}						
-				  })
-				  .change();
+		for (i = 12; i > 0; i--) {
+			$("#players").append($("<option></option>").val(i).html(i));
+		}
+		var groups = [1,2,3];
+
+		$("#players")
+		  .change(function () {
+			$('#playersList').empty();
+			for (i = $(this).val(); i > 0; i--) {
+				$('#playersList').append('<div name="player"><label for="player">Player</label><select name="player_id[]" class="player ui-corner-all" id="player"></select><label for="level">Level</label><select name="level_id[]" class="level ui-corner-all" id="level" ></select><label for="group">Group</label><select name="group[]" class="group ui-corner-all" id="group" ></select></div><br />'); //add player div
+			}
+			$.each(players, function(index, text) {
+				$(".player").append(
+					$("<option></option>").val(text.id).html(text.name)
+				);
+			});
+			$.each(levels, function(index, text) {
+				$(".level").append(
+					$("<option></option>").val(text.id).html(text.name)
+				);
+			});
+			for (i = 0; i < groups.length; i++) {
+				$(".group").append(
+					$("<option></option>").val(groups[i]).html(groups[i])
+				);
+			}
+		  }).change();
             });
             function submitScoreForm() {
                 $.ajax({
