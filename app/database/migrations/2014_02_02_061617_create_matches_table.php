ba43c29772cfc448ec2edd121c76c87f533a6aca
Michael Schmidt <mschmidt74@hotmail.com>
2015-02-18 16:26:11 -0500

    Update 2014_02_02_061617_create_matches_table.php
    Moved money table elements into match table

--- app/database/migrations/2014_02_02_061617_create_matches_table.php
+++ app/database/migrations/2014_02_02_061617_create_matches_table.php
@@ -16,6 +16,11 @@ class CreateMatchesTable extends Migration {
 			$table->increments('id');
 			$table->date('date')->unique();
 			$table->integer('course_id')->unsigned()->index();
+			$table->decimal('purse', 2);
+			$table->decimal('skinsamoney', 2);
+			$table->decimal('skinsbmoney', 2);
+			$table->decimal('grossmoney', 2);
+			$table->decimal('netmoney', 2);
 
 			$table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
 
