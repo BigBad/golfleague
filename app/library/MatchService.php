230e75baac72ce6fba3f81f7587784008991e6c1
Michael Schmidt <mschmidt74@hotmail.com>
2015-02-20 16:16:16 -0500

    Update MatchService.php

--- app/library/MatchService.php
+++ app/library/MatchService.php
@@ -1,1 +1,55 @@
 
+<?php namespace GolfLeague\Services;
+
+use GolfLeague\Storage\Match\MatchRepository;
+use GolfLeague\PrizeMoney;
+use \Player;
+/**
+* Our MatchService, containing all useful methods for business logic around Matches
+*/
+class MatchService
+{
+    // Containing our pokemonRepository to make all our database calls to
+    protected $matchRepo;
+
+    /**
+    * Loads our $matchRepo
+    *
+    * @param MatchRepository $matchRepo
+    * @return MatchService
+    */
+    public function __construct(MatchRepository $matchRepo, PrizeMoney $prizeMoney, Player $player)
+    {
+        $this->matchRepo = $matchRepo;
+        $this->prizeMoney = $prizeMoney;
+        $this->player = $player;
+    }
+
+    /**
+    * Method to creat match from input Match date
+    *
+    * @param mixed $matchdata
+    * @return string
+    */
+    public function create($matchdata)
+    {
+
+        //calculate money with purse
+        $this->prizeMoney->setPurse($matchdata['purse']);
+
+        $matchdata['purse'] = money_format('%i', $matchdata['purse']);
+        $matchdata['grossmoney'] = $this->prizeMoney->getlowScore();
+        $matchdata['netmoney'] = $this->prizeMoney->getlowScore();
+        $matchdata['skinsamoney'] = $this->prizeMoney->getSkins();
+        $matchdata['skinsbmoney'] = $this->prizeMoney->getSkins();
+
+        foreach ($matchdata['player'] as $key=>$player) {
+            //get each player's current handicap
+            $currentPlayer = $this->player->find($player['player_id']);
+            $matchdata['player'][$key]['handicap'] = $currentPlayer->handicap;
+            $matchdata['player'][$key]['winnings'] = 0;
+        }// End foreach
+        return $matchdata;
+        return $this->matchRepo->create($matchdata);
+    }
+}
