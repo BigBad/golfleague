b7ef78245b72704281ccf02fbd2dc5ba55f5aaf9
Michael Schmidt <mschmidt74@hotmail.com>
2015-02-27 10:39:57 -0500

    Update HolescoresTableSeeder.php

--- app/database/seeds/HolescoresTableSeeder.php
+++ app/database/seeds/HolescoresTableSeeder.php
@@ -944,6 +944,7 @@ class HolescoresTableSeeder extends Seeder {
         Holescore::create(array('id' => '943','score' => '4','hole_id' => '16','round_id' => '253','created_at' => '2014-06-11 01:37:56','updated_at' => '2014-06-11 01:37:56'));
         Holescore::create(array('id' => '944','score' => '5','hole_id' => '17','round_id' => '253','created_at' => '2014-06-11 01:37:56','updated_at' => '2014-06-11 01:37:56'));
         Holescore::create(array('id' => '945','score' => '6','hole_id' => '18','round_id' => '253','created_at' => '2014-06-11 01:37:56','updated_at' => '2014-06-11 01:37:56'));
+        DB::statement('ALTER SEQUENCE holescores_id_seq RESTART WITH 946;');
     }
 
 }
