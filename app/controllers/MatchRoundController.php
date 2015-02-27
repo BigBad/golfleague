394fae6273984fb5cbe3313d72609977c91ed60f
Michael Schmidt <mschmidt74@hotmail.com>
2015-02-27 14:50:39 -0500

    Update and rename app/controllers to app/controllers/MatchRoundController.php

---
+++ app/controllers/MatchRoundController.php
@@ -0,0 +1,93 @@
+<?php
+
+use GolfLeague\Services\MatchRoundService as MatchRoundService;
+
+class MatchRoundController extends \BaseController {
+
+	    public function __construct(MatchRoundService $matchRound)
+    {
+        $this->matchRound = $matchRound;
+    }
+
+    /**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return $this->matchRound->all();
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		return $this->matchRound->getByMatch($id);
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
