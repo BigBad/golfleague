c3cb086b40bfc5b15998466e9c13bb86d54c18ef
Michael Schmidt <mschmidt74@hotmail.com>
2015-03-02 13:07:29 -0500

    Update Round.php

--- app/models/Round.php
+++ app/models/Round.php
@@ -2,12 +2,19 @@
 
 class Round extends Eloquent
 {
+	protected $guarded = array('id');
+	protected $fillable = array('date', 'player_id', 'course_id', 'match_id', 'score', 'esc');
+
+	public function setDateAttribute($date)
+    {
+		$this->attributes['date'] = date('Y-m-d', strtotime(str_replace('-', '/', $date)));
+    }
 
     public function player()
     {
         return $this->belongsTo('Player');
     }
-	
+
 	public function course()
     {
         return $this->belongsTo('Course');
@@ -15,7 +22,7 @@ class Round extends Eloquent
 
     public function holescores()
     {
-        return $this->hasMany('Holescore');
+        return $this->hasMany('Holescore')->orderBy('id');
     }
 
     public function holes()
