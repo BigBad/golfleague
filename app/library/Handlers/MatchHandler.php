44c60ef258938e0133a6f350c4ca5fe571cbb0e1
Michael Schmidt <mschmidt74@hotmail.com>
2015-03-04 11:09:14 -0500

    Update and rename app/library to app/library/Handlers/MatchHandler.php

---
+++ app/library/Handlers/MatchHandler.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace GolfLeague\Handlers;
+
+use GolfLeague\Storage\Round\RoundRepository;
+
+/**
+ * MatchHandler Connection Class
+ *
+ * This class subscribes to events in related to match creation
+ *
+ * @author          Michael Schmidt
+ */
+class MatchHandler
+{
+    /**
+     * Create a new instance of the MatchHandler
+     *
+     * @param  GolfLeague\Storage\Round\RoundRepository $roundRepo
+     * @return void
+     */
+    public function __construct(RoundRepository $roundRepo)
+    {
+        $this->roundRepo = $roundRepo;
+    } // End of __construct
+
+    /**
+     * Create an initial round for each player after a new match is created
+     *
+     * @param  Match $match
+     * @return void
+     */
+    public function handle($match)
+    {
+        //for each player create an initial round
+
+        $input = array(
+                'date' => $match['date'],
+                'course_id' => $match['course'],
+                'match_id' => $match['match_id'],
+                'score' => 0,
+                'esc' =>0
+        );
+        foreach($match['player'] as $player){
+            $input['player_id'] = $player['player_id'];
+            $this->roundRepo->create($input);
+        }
+    }
+
+    /**
+     * Register the listeners for the subscriber.
+     *
+     * @param  Illuminate\Events\Dispatcher $events
+     * @return array
+     */
+    public function subscribe($events)
+    {
+        $events->listen('match.create', 'GolfLeague\Handlers\MatchHandler');
+    }
+}
