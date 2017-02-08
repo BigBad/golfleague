87faf2fd93bbf73243fb2c51cac9f879f8ab768f
Michael Schmidt <Michael.Schmidt@ABILITYNetwork.com>
2017-02-08 16:04:33 -0500

    Declare private variables for consistency

--- app/library/Classes/Handicap.php
+++ app/library/Classes/Handicap.php
@@ -7,6 +7,10 @@ use \Round as Round;
 
 class Handicap
 {
+    private $differentialArray;
+    private $player;
+    private $roundsUsed;
+    
 	public function __construct(Player $player, $roundsUsed = 20)
     {
         $this->differentialArray = array(0,1,1,1,1,1,1,2,2,3,3,4,4,5,5,6,6,7,8,9,10);
